1b8cc4992c1dd79b5cfac7d56b4bcd95215e21ef
José Eduardo Ferreira <54028051+Ferreira3@users.noreply.github.com>
2025-09-30 21:02:44 -0300

    Adiciona funcionalidades ao menu produtos

--- src/products.py
+++ src/products.py
@@ -1,10 +1,10 @@
 from os import system
 from time import sleep
 
+products = {}
+
 
 def products_menu(products_database):  # função que controla o menu dos produtos
-    products = {}  # noqa
-
     while True:
         system("cls")
         print(
@@ -20,24 +20,20 @@ def products_menu(products_database):  # função que controla o menu dos produt
         choice = input("Escolha uma opção: ")
 
         if choice == "1":
-            product = create_product()
-            products_database.append(product)
-
+            create_product()
+            products_database.append(products.copy())
         elif choice == "2":
-            ...
-
+            show_products(products_database)
         elif choice == "3":
-            ...
-
+            search_product_byid(products_database)
         elif choice == "4":
             ...
-
+            # update_productid()
         elif choice == "5":
-            ...
-
+            delete_product_byid(products_database)
         elif choice == "6":
             ...
-
+            # import_product()
         elif choice == "7":
             return products_database
         else:
@@ -46,4 +42,68 @@ def products_menu(products_database):  # função que controla o menu dos produt
 
 
 def create_product():
-    return {}
+    system("cls")
+    print("=" * 52)
+    print(f"{"CADASTRAR PRODUTO":^52}")
+    print("=" * 52)
+    products["name"] = str(input("Nome do produto: ")).strip().title()
+    products["price"] = float(input("Valor do produto: R$"))
+    input("\nProduto cadastrado! Pressione qualquer tecla para continuar.")
+
+
+def show_products(database):
+    system("cls")
+    print("=" * 52)
+    print(f"{"LISTA DE PRODUTOS":^52}")
+    print("=" * 52)
+    if len(database) == 0:
+        print("Lista de produtos vazia!")
+    else:
+        print(f"{'No.':<5}{'Produto':<40}{'Valor'}")
+        for index, item in enumerate(database):
+            print(f"{index:<5}{item["name"]:.<40}R${item["price"]:.2f}")
+    input("\nPressione qualquer tecla para voltar.")
+
+
+def search_product_byid(database):
+    system("cls")
+    print("=" * 52)
+    print(f"{"BUSCAR PRODUTO":^52}")
+    print("=" * 52)
+    search = int(input("Digite o ID do produto: "))
+    if search > len(database)-1 or search < 0:
+        input("\nProduto não encontrado! Pressione qualquer tecla para continuar.")
+    else:
+        print(f"{'No.':<5}{'Produto':<40}{'Valor'}")
+        print(f"{search:<5}{database[search]["name"]:.<40}R${database[search]["price"]:.2f}")
+        input("\nPressione qualquer tecla para voltar.")
+
+
+def update_productid(database):
+    ...
+
+
+def delete_product_byid(database):
+    system("cls")
+    print("=" * 52)
+    print(f"{"EXCLUIR PRODUTO":^52}")
+    print("=" * 52)
+    search = int(input("Digite o ID do produto a ser excluído: "))
+    if search > len(database)-1 or search < 0:
+        input("\nProduto não encontrado! Pressione qualquer tecla para continuar.")
+    else:
+        print(f"{'No.':<5}{'Produto':<40}{'Valor'}")
+        print(f"{search:<5}{database[search]["name"]:.<40}R${database[search]["price"]:.2f}")
+        while True:
+            resp = input(f"\nTem certeza que deseja excluir {database[search]["name"]}?[S/N]").strip().lower()[0]
+            if 'n' in resp:
+                input("\nOperação cancelada! Pressione qualquer tecla para continuar.")
+                break
+            if 's' in resp:
+                del database[search]
+                input("\nProduto excluído! Pressione qualquer tecla para continuar.")
+                break
+
+
+def import_product():
+    ...
